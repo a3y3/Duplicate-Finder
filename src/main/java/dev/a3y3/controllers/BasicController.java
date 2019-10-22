b3f94fc39fccb3baae77eb18510acf53ae1dbab5
a3y3 <sohamssd@gmail.com>
2019-10-21 21:07:59 -0400

    Add a basic implementation of isPossibleDuplicate

--- src/main/java/dev/a3y3/controllers/BasicController.java
+++ src/main/java/dev/a3y3/controllers/BasicController.java
@@ -50,8 +50,8 @@ public class BasicController {
     }
 
     private void fillResults(List<String> result, List<String> rows) {
-        for (int i = 0; i < rows.size() - 1; i ++){
-            for (int j = i + 1; j < rows.size(); j++){
+        for (int i = 0; i < rows.size() - 1; i++) {
+            for (int j = i + 1; j < rows.size(); j++) {
                 String row1 = rows.get(i);
                 String row2 = rows.get(j);
 
@@ -59,7 +59,39 @@ public class BasicController {
         }
     }
 
-    private boolean isPossibleDuplicate(String str1, String str2){
+    /**
+     * Checks if rows row1 and row2 are a possible duplicate of each other.
+     * The function checks for all the columns in {@code row1} with the matching column
+     * of {@code row2}. It determines that the entire row is a possible duplicate iff:
+     * <p>
+     * 1. The LevenshteinDistance between them is less than or equal to the
+     * threshold for the particular column (defined in Constants.java):
+     * {@see int getLevenshteinDistance(String str1, String str2)}
+     * 2. They are Metaphones of each other:
+     * {@see boolean isMetaphoneEqual(String str1, String str2)}
+     *
+     * @param row1 the String representation (comma separated) of a row.
+     * @param row2 the String representation (comma separated) of a row.
+     * @return true if {@code row1} is a possible duplicate of {@code row2}.
+     */
+    private boolean isPossibleDuplicate(String row1, String row2) {
+        String[] colsOfFirst = row1.split(",");
+        String[] colsOfSecond = row2.split(",");
+        //now, rowsOfFirst[Constants.OFFSET_xxx] will refer to the appropriate column.
+
+        //Assume for now that both the lengths are the same.
+        for (int i = 1; i < colsOfFirst.length; i++) {
+            String column = constants.columnNames[i];
+            int threshold = constants.thresholds.get(column);
+
+            //Variables value1 and value2 denote the values of the respective columns.
+            String value1 = colsOfFirst[i];
+            String value2 = colsOfSecond[i];
+            if (isMetaphoneEqual(value1, value2) || getLevenshteinDistance(value1,
+                    value2) <= threshold) {
+                return true;
+            }
+        }
         return false;
     }
 
