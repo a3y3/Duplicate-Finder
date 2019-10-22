6fb5b9ec770040dd0d45de163155f76bcaebe352
a3y3 <sohamssd@gmail.com>
2019-10-21 20:58:00 -0400

    Modify thresholds in Constants to be a single HashMap for allowing quering a threshold by name

--- src/main/java/dev/a3y3/Constants.java
+++ src/main/java/dev/a3y3/Constants.java
@@ -1,31 +1,41 @@
 package dev.a3y3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Soham Dongargaonkar [sd4324] on 10/21/19
  */
 public class Constants {
-    public static final int OFFSET_ID = 0;
-    public static final int OFFSET_FNAME = 1;
-    public static final int OFFSET_LNAME = 2;
-    public static final int OFFSET_COMPANY = 3;
-    public static final int OFFSET_EMAIL = 4;
-    public static final int OFFSET_ADDR1 = 5;
-    public static final int OFFSET_ADDR2 = 6;
-    public static final int OFFSET_ZIP = 7;
-    public static final int OFFSET_CITY = 8;
-    public static final int OFFSET_STATE_LONG = 9;
-    public static final int OFFSET_STATE_SHORT = 10;
-    public static final int OFFSET_PHONE = 11;
+    public final int OFFSET_ID = 0;
+    public final int OFFSET_FNAME = 1;
+    public final int OFFSET_LNAME = 2;
+    public final int OFFSET_COMPANY = 3;
+    public final int OFFSET_EMAIL = 4;
+    public final int OFFSET_ADDR1 = 5;
+    public final int OFFSET_ADDR2 = 6;
+    public final int OFFSET_ZIP = 7;
+    public final int OFFSET_CITY = 8;
+    public final int OFFSET_STATE_LONG = 9;
+    public final int OFFSET_STATE_SHORT = 10;
+    public final int OFFSET_PHONE = 11;
 
-    public static final int THRESHOLD_NAME = 2;
-    public static final int THRESHOLD_COMPANY = 2;
-    public static final int THRESHOLD_EMAIL = 2;
-    public static final int THRESHOLD_ADDR1 = 2;
-    public static final int THRESHOLD_ADDR2 = 2;
-    public static final int THRESHOLD_ZIP = 2;
-    public static final int THRESHOLD_CITY = 2;
-    public static final int THRESHOLD_STATE_LONG = 2;
-    public static final int THRESHOLD_STATE_SHORT = 2;
-    public static final int THRESHOLD_PHONE = 2;
+    public final Map<String, Integer> thresholds = new HashMap<>();
+    public final String[] columnNames = new String[]{"id", "first_name", "last_name",
+            "company", "email", "address1", "address2", "zip", "city", "state_long",
+            "state", "phone"};
 
+    public Constants() {
+        thresholds.put("first_name", 2);
+        thresholds.put("last_name", 2);
+        thresholds.put("company", 2);
+        thresholds.put("email", 2);
+        thresholds.put("address1", 2);
+        thresholds.put("address2", 2);
+        thresholds.put("zip", 2);
+        thresholds.put("city", 2);
+        thresholds.put("state_long", 2);
+        thresholds.put("state", 2);
+        thresholds.put("phone", 2);
+    }
 }
--- src/main/java/dev/a3y3/controllers/BasicController.java
+++ src/main/java/dev/a3y3/controllers/BasicController.java
@@ -1,9 +1,6 @@
 package dev.a3y3.controllers;
 
 import dev.a3y3.Constants;
-import org.apache.commons.codec.language.Metaphone;
-import org.apache.commons.text.similarity.LevenshteinDistance;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +15,7 @@ import java.util.List;
  */
 @Controller
 public class BasicController {
-
-
+    private final Constants constants = new Constants();
 
     @GetMapping("/")
     public String index(@RequestParam(name = "fileName", required = false, defaultValue
@@ -29,6 +25,7 @@ public class BasicController {
         model.addAttribute("results", results);
 
         List<String> fileRows = getFileRows(fileName);
+        fillResults(results, fileRows);
         return "index";
     }
 
@@ -52,11 +49,25 @@ public class BasicController {
         return rows;
     }
 
-    private int getLevenshteinDistance(String a, String b){
+    private void fillResults(List<String> result, List<String> rows) {
+        for (int i = 0; i < rows.size() - 1; i ++){
+            for (int j = i + 1; j < rows.size(); j++){
+                String row1 = rows.get(i);
+                String row2 = rows.get(j);
+
+            }
+        }
+    }
+
+    private boolean isPossibleDuplicate(String str1, String str2){
+        return false;
+    }
+
+    private int getLevenshteinDistance(String str1, String str2) {
         return 0;
     }
 
-    private boolean isMetaphoneEqual(String str1, String str2){
+    private boolean isMetaphoneEqual(String str1, String str2) {
         return false;
     }
 }
